970294f5fcc1463cde0a55b72bc5129ab3f0c3f3
evilpauwse1 <evilpauwse@yahoo.com>
2020-08-31 15:56:16 -0400

    token checking, first request test

--- Paths/fetchPlayer.js
+++ Paths/fetchPlayer.js
@@ -1,4 +1,5 @@
 exports.run = function(player,platform) {
+    let request = require("request");
     if(process.env.tk === "" || process.env.tk === undefined) {
         console.error(`Missing token, please set a token with rocketleagueapi.token("TOKEN HERE")`);
         return process.exit(1);
@@ -10,5 +11,15 @@ exports.run = function(player,platform) {
     } catch(e) {
         return console.error("Invalid parameters, Player and Platform must be strings.");
     }
-
+    let options = {
+        'method': 'GET',
+        'url': `api.rocketleague.com/api/v1/${platform}/playerskills/${player}`,
+        'headers': {
+          'Authorization': `Token ${process.env.tk}`
+        }
+    }
+    request(options, function(err, res) {
+        if(err) return err;
+        return res.body
+    })
 }
