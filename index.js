9c35a791af0207f633332df00a1c63a3318f7dec
evilpauwse1 <evilpauwse@yahoo.com>
2020-08-31 15:47:46 -0400

    Update for process changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ exports.token = function(token) {
         console.error(`Invalid token specified. Please double check the token you entered.`);
         return process.exit()
     }
-    return tk = token.trim();
+    return process.env.tk = token.trim();
     //Token set
 }
 exports.getRank = function(player, platform) {
